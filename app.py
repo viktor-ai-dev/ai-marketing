7a587bb1df608771fdce74aa423a072492a71dfb
Viktor <viktor.sz@tutamail.com>
2026-04-07 16:29:45 +0200

    Minor updates

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 from modules.prompts import marketing_prompt
 from modules.generator import generate_campaing
 
-
 st.title("🚀 AI Marketing Automation")
 
 product = st.text_input("Prouct Name")
 category = st.text_input("Category")
 features = st.text_area("Features")
 
+result = None
 if st.button("Generate Full Campaign"):
     
     # Returns full marketing info(description, etc) for a campaign
@@ -16,14 +16,12 @@ if st.button("Generate Full Campaign"):
 
     with st.spinner(text="Generating full campaign..."):
         result = generate_campaing(prompt=prompt)
-
         st.markdown("## 📊 Campaign Output")
         st.write(result)
 
-        if st.button("Save Campaign"):
-            with open("campaign.txt", "w") as f:
-                f.write(result)
-
+if st.button("Save Campaign") and result:
+    with open("campaign.txt", "w") as f:
+        f.write(result)
 
 st.markdown("### 🛍 Product Description")
 st.write("section_1")
--- modules/generator.py
+++ modules/generator.py
@@ -1,5 +1,8 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
